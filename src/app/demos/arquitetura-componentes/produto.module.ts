993a74b210fe1184d8821c681e264ff14b4a500c
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-10-09 17:00:14 -0300

    @Input decorator

--- src/app/demos/arquitetura-componentes/produto.module.ts
+++ src/app/demos/arquitetura-componentes/produto.module.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
+import { registerLocaleData } from "@angular/common";
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
 import { ProdutoDashboardComponent } from "./produto-dashboard/produto-dashboard.component";
 import { ProdutoRountingModule } from "./produto.route";
+import { ProdutoDetalheComponent } from "./components/produto-card-detalhe.component";
+import { ProdutoCountComponent } from "./components/produto-count.component";
 
 
 @NgModule({
     declarations: [
-        ProdutoDashboardComponent
+        ProdutoDashboardComponent,
+        ProdutoDetalheComponent,
+        ProdutoCountComponent
     ],
     imports: [
         CommonModule,
