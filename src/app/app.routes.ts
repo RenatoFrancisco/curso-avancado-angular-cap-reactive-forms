141bbbfcc553ad7c1a39f85b3902ce82706be497
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-10-11 22:11:01 -0300

    Tratamento de erro 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './navegacao/home/home.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
+import { NotFoundComponent } from './navegacao/not-found/not-found.component';
 
 const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -12,7 +13,9 @@ const rootRouterConfig: Routes = [
     { path: 'cadastro', component: CadastroComponent },
     { path: 'produtos', 
             loadChildren: () => import('./demos/arquitetura-componentes/produto.module')
-            .then(m => m.ProdutoModule)}
+            .then(m => m.ProdutoModule)},
+
+    { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
